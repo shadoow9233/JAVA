c15e5758511ee2b6ebba35bb443f9e754df4c064
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-01-19 10:46:52 +0545

    variables in java

--- src/Day1/javaBasics.java
+++ src/Day1/javaBasics.java
@@ -5,5 +5,24 @@ public class javaBasics {
         System.out.println("Hello, World! from learning java");
         System.out.println("Print ln start from new line");
         System.out.printf("printf start from same line ");
+        //this is a single comment
+        /* The code below will print the words Hello World
+        to the screen, and it is amazing */
+
+
+        //variables
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        // Printing variables
+        System.out.println("myNum: " + myNum);
+        System.out.println("myFloatNum: " + myFloatNum);
+        System.out.println("myLetter: " + myLetter);
+        System.out.println("myBool: " + myBool);
+        System.out.println("myText: " + myText);
+
     }
 }
