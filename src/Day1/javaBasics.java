53a7ad3f6e9ac3c66cf8c3d91865458174ee82d3
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-01-19 10:18:54 +0545

    start java learning

---
+++ src/Day1/javaBasics.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class javaBasics {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! from learning java");
+        System.out.println("Print ln start from new line");
+        System.out.printf("printf start from same line ");
+    }
+}
